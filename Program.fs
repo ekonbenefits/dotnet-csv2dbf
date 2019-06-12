152a44d3f9c4181a8b1831728db0458de7d9c8c9
jbtule <jay.tuley@ekonbenefits.com>
2019-06-12 14:27:31 -0500

    Clean up info

--- Program.fs
+++ Program.fs
@@ -1,4 +1,18 @@
-﻿
+﻿(*
+  Copyright 2019 Ekon Benefits
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*)
 open System
 open McMaster.Extensions.CommandLineUtils
 open System.IO
@@ -103,9 +117,9 @@ let main argv =
     use app = new CommandLineApplication();
     app.HelpOption() |> ignore;
     let reqTemplate = app.Option<string>("-d|--dbf <DBF_TEMPLATE>", "The DBF Template", CommandOptionType.SingleValue).IsRequired()
-    let optInputEncoding = app.Option<string>("-i|--input-encoding <ENCODING>", "Input Encoding", CommandOptionType.SingleValue) 
-    let optOutDir = app.Option<string>("-o|--output <DIRECTORY>", "Output Directory", CommandOptionType.SingleValue)
-    let optFileName = app.Option<string>("-f|--filename <OUTPUT_FILENAME>", "Output Filename", CommandOptionType.SingleValue)
+    let optInputEncoding = app.Option<string>("-i|--input-encoding <ENCODING>", "Input Encoding [Default: Detect]", CommandOptionType.SingleValue) 
+    let optOutDir = app.Option<string>("-o|--output <DIRECTORY>", "Output Directory [Default: Current directory]", CommandOptionType.SingleValue)
+    let optFileName = app.Option<string>("-f|--filename <OUTPUT_FILENAME>", "Output Filename [Default: <INPUT>.dbf]", CommandOptionType.SingleValue)
     let optOutputEncoding = app.Option<string>("-e|--output-encoding <ENCODING>", "Output Encoding [Default: utf-8]", CommandOptionType.SingleValue)
     let input = app.Argument<string>("Input", "CSV Files to convert", multipleValues=true).IsRequired();
 
